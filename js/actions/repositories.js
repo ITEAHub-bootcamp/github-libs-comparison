f2966144fab59c62d365419f1a4c12acc8a50d89
Vladimir <dosandkv@gmail.com>
2017-09-10 15:47:50 +0300

    project structure refactoring;

--- js/actions/repositories.js
+++ js/actions/repositories.js
@@ -2,15 +2,19 @@ import {LOAD} from '../constants';
 import apiMock from '../mocks/index';
 
 export const load = () => {
-  // request to some API
   return (dispatch, getState) => {
-    return apiMock.get('search?foo').then(repos => {
-      const repositories = repos.repositories.items.slice(0, 5);
+    // return apiMock.get('search?foo').then(repos => {
+    // const url = 'https://api.github.com/search/repositories?q=react';
+    // return fetch(url).then(data => data.json());
+    return apiMock.get('search?foo')
+      .then(repos => {
+        const repositories = repos.repositories.items.slice(0, 5);
 
-      return dispatch({
-        type: LOAD,
-        payload: {repositories}
-      })
-    })
+        return dispatch({
+          type: LOAD,
+          payload: {repositories}
+        })
+      }
+    )
   };
 };
